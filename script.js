7a8c73b55931fd47283964a3d4a3b6f2b706a3a8
HilaryLanham <h26200@hotmail.com>
2021-09-29 15:29:03 +0100

    array exercise

--- script.js
+++ script.js
@@ -52,4 +52,25 @@ for(a; a <= 200; a++){
 }
 
  //
+//  1. Create an array with the name myArray with 2 elements hello,everyone..
+
+let myArray = [`hello`, `everyone`];
+
+//  2. Next print the length of the array
+//logs the number of things in array
+console.log(myArray.length);
  
+//  3. Next use the push() method to add 3 elements to the array
+myArray.push(`goodbye`)
+myArray.push(`yo`)
+myArray.push(`au revoir`)
+ 
+//  4. Next print the length of the array
+console.log(myArray.length);
+
+//  5. Next use shift() to remove an element
+myArray.shift()
+ 
+//  6. Finally print the contents of the array using a for of loop. 
+for (let list of myArray)
+{console.log(list);}
